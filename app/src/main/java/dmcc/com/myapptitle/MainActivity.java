56fccac2eb8e1d3bdc7f8ea8dfefcb3fd7ef04a4
wushange <309646776@qq.com>
2016-12-19 17:27:02 +0800

    说明

--- app/src/main/java/dmcc/com/myapptitle/MainActivity.java
+++ app/src/main/java/dmcc/com/myapptitle/MainActivity.java
@@ -10,20 +10,40 @@ import com.wsg.widget.AppTitle;
 public class MainActivity extends AppCompatActivity {
 
     AppTitle commonBar;
+    Toast mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         commonBar = (AppTitle) findViewById(R.id.mytitle);
+        commonBar.setCenterText("标题").setShowLeftButton(true).setCanFinish(true).setRightText("保存");
         commonBar.setActionListener(new AppTitle.ActionListener() {
             @Override
             public void OnActionClickListener(View view) {
+                showToast("--" + view.getId());
+            }
 
-                Toast.makeText(MainActivity.this,"--" + view.getId(),Toast.LENGTH_SHORT).show();
-
+            @Override
+            public boolean OnActionLongClickListener(View view) {
+                switch (view.getId()) {
+                    case R.id.title_right_text:
+                        showToast("保存");
+                        break;
+                }
+                return true;
             }
         });
 
+    }
+
+    public void showToast(CharSequence text) {
+        if (mToast == null) {
+            mToast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+        } else {
+            mToast.setText(text);
+            mToast.setDuration(Toast.LENGTH_SHORT);
+        }
+        mToast.show();
     }
 }
--- apptitle/src/main/java/com/wsg/widget/AppTitle.java
+++ apptitle/src/main/java/com/wsg/widget/AppTitle.java
@@ -18,7 +18,7 @@ import android.widget.TextView;
  * Created by wushange on 2016/12/19.
  */
 
-public class AppTitle extends RelativeLayout implements View.OnClickListener {
+public class AppTitle extends RelativeLayout implements View.OnClickListener, View.OnLongClickListener {
     private RelativeLayout mRoot;
     private ImageButton mLeftButton;
     private ImageButton mLeftImage;
@@ -28,13 +28,171 @@ public class AppTitle extends RelativeLayout implements View.OnClickListener {
     private ImageButton mRightButton2;
     private TextView mRightText;
 
-    ActionListener actionListener;
+    private ActionListener actionListener;
+    private boolean canFinish = true;
 
     public AppTitle(Context context, AttributeSet attrs) {
         super(context, attrs);
         initView(context);
         initAttrs(context, attrs);
 
+    }
+
+
+    public void setActionListener(ActionListener actionListener) {
+        this.actionListener = actionListener;
+    }
+
+
+    private void initView(Context context) {
+        LayoutInflater.from(context).inflate(R.layout.title, this);
+        mRoot = (RelativeLayout) findViewById(R.id.title_root);
+        mCenterText = (TextView) findViewById(R.id.title_center_text);
+        mLeftButton = (ImageButton) findViewById(R.id.title_back_btn);
+        mLeftImage = (ImageButton) findViewById(R.id.title_left_img);
+        mLeftText = (TextView) findViewById(R.id.title_left_text);
+        mRightButton = (ImageButton) findViewById(R.id.title_right_btn);
+        mRightButton2 = (ImageButton) findViewById(R.id.title_right_btn_sub);
+        mRightText = (TextView) findViewById(R.id.title_right_text);
+        mLeftButton.setOnClickListener(this);
+        mLeftImage.setOnClickListener(this);
+        mCenterText.setOnClickListener(this);
+        mRightText.setOnClickListener(this);
+        mRightButton.setOnClickListener(this);
+        mRightButton2.setOnClickListener(this);
+        mLeftButton.setOnLongClickListener(this);
+        mLeftImage.setOnLongClickListener(this);
+        mCenterText.setOnLongClickListener(this);
+        mRightText.setOnLongClickListener(this);
+        mRightButton.setOnLongClickListener(this);
+        mRightButton2.setOnLongClickListener(this);
+
+    }
+
+    public void initAttrs(Context context, AttributeSet attrs) {
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.AppTitle);
+        int totalAttributes = typedArray.getIndexCount();
+        for (int i = 0; i < totalAttributes; i++) {
+            int index = typedArray.getIndex(i);
+            if (index == R.styleable.AppTitle_showBack) {
+                mLeftButton.setVisibility(getBoolean(typedArray, index, R.bool.default_showBack) ? VISIBLE : INVISIBLE);
+            } else if (index == R.styleable.AppTitle_canFinish) {
+                canFinish = getBoolean(typedArray, index, R.bool.default_canFinish);
+            } else if (index == R.styleable.AppTitle_leftButtonRes) {
+                mLeftButton.setImageResource(getResourceId(typedArray, index, R.drawable.ic_launcher));
+            } else if (index == R.styleable.AppTitle_leftImageRes) {
+                mLeftImage.setVisibility(VISIBLE);
+                mLeftImage.setImageResource(getResourceId(typedArray, index, R.drawable.ic_launcher));
+            } else if (index == R.styleable.AppTitle_leftText) {
+                mLeftText.setVisibility(VISIBLE);
+                mLeftText.setText(getString(typedArray, index));
+            } else if (index == R.styleable.AppTitle_leftTextColor) {
+                mLeftText.setTextColor(getColor(typedArray, index, R.color.default_leftTextColor));
+            } else if (index == R.styleable.AppTitle_leftTextSize) {
+                mLeftText.setTextSize(TypedValue.COMPLEX_UNIT_PX, getDimenSize(typedArray, index, R.dimen.default_leftTextSize));
+            } else if (index == R.styleable.AppTitle_centerText) {
+                mCenterText.setVisibility(VISIBLE);
+                mCenterText.setText(getString(typedArray, index));
+            } else if (index == R.styleable.AppTitle_centerTextColor) {
+                mCenterText.setTextColor(getColor(typedArray, index, R.color.default_centerTextColor));
+            } else if (index == R.styleable.AppTitle_centerTextSize) {
+                mCenterText.setTextSize(TypedValue.COMPLEX_UNIT_PX, getDimenSize(typedArray, index, R.dimen.default_centerTextSize));
+            } else if (index == R.styleable.AppTitle_rightText) {
+                mRightText.setVisibility(VISIBLE);
+                mRightText.setText(getString(typedArray, index));
+            } else if (index == R.styleable.AppTitle_rightTextColor) {
+                mRightText.setTextColor(getColor(typedArray, index, R.color.default_rightTextColor));
+            } else if (index == R.styleable.AppTitle_rightTextSize) {
+                mRightText.setTextSize(TypedValue.COMPLEX_UNIT_PX, getDimenSize(typedArray, index, R.dimen.default_rightTextSize));
+            } else if (index == R.styleable.AppTitle_rightButtonRes) {
+                mRightButton.setVisibility(VISIBLE);
+                mRightButton.setImageResource(getResourceId(typedArray, index, R.drawable.ic_launcher));
+            } else if (index == R.styleable.AppTitle_rightButton2Res) {
+                mRightButton2.setVisibility(VISIBLE);
+                mRightButton2.setImageResource(getResourceId(typedArray, index, R.drawable.ic_launcher));
+            }
+
+        }
+        typedArray.recycle();
+    }
+
+
+    public AppTitle setLeftText(String leftText) {
+        mLeftText.setVisibility(VISIBLE);
+        mLeftText.setText(leftText);
+        return this;
+    }
+
+
+    public AppTitle setCenterText(String centerText) {
+        mCenterText.setVisibility(VISIBLE);
+        mCenterText.setText(centerText);
+        return this;
+    }
+
+    public AppTitle setRightText(String rightText) {
+        mRightText.setVisibility(VISIBLE);
+        mRightText.setText(rightText);
+        return this;
+    }
+
+    public AppTitle setLeftButtonRes(int res) {
+        mLeftButton.setVisibility(VISIBLE);
+        mLeftButton.setImageResource(res);
+        return this;
+    }
+
+
+    public AppTitle setLeftImageRes(int res) {
+        mLeftImage.setVisibility(VISIBLE);
+        mLeftImage.setImageResource(res);
+        return this;
+    }
+
+    public AppTitle setRightButtonRes(int res) {
+        mRightButton.setVisibility(VISIBLE);
+        mRightButton.setImageResource(res);
+        return this;
+    }
+
+    public AppTitle setRightButton2Res(int res) {
+        mRightButton2.setVisibility(VISIBLE);
+        mRightButton2.setImageResource(res);
+        return this;
+    }
+
+    public AppTitle setShowLeftButton(boolean showBack) {
+        mLeftButton.setVisibility(showBack ? VISIBLE : INVISIBLE);
+        return this;
+    }
+
+    public AppTitle setCanFinish(boolean canFinish) {
+        this.canFinish = canFinish;
+        return this;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (actionListener != null) {
+            if (canFinish) {
+                if (view.getId() == R.id.title_back_btn) {
+                    ((Activity) getContext()).finish();
+                }
+            }
+            actionListener.OnActionClickListener(view);
+        }
+
+    }
+
+    @Override
+    public boolean onLongClick(View view) {
+        return actionListener == null ? false : actionListener.OnActionLongClickListener(view);
+    }
+
+
+    public AppTitle setLeftText() {
+        return this;
     }
 
     public RelativeLayout getmRoot() {
@@ -69,86 +227,11 @@ public class AppTitle extends RelativeLayout implements View.OnClickListener {
         return mRightText;
     }
 
-    public void setActionListener(ActionListener actionListener) {
-        this.actionListener = actionListener;
-    }
-
-    private void initView(Context context) {
-        LayoutInflater.from(context).inflate(R.layout.title, this);
-        mRoot = (RelativeLayout) findViewById(R.id.title_root);
-        mCenterText = (TextView) findViewById(R.id.title_center_text);
-        mLeftButton = (ImageButton) findViewById(R.id.title_back_btn);
-        mLeftImage = (ImageButton) findViewById(R.id.title_left_img);
-        mLeftText = (TextView) findViewById(R.id.title_left_text);
-        mRightButton = (ImageButton) findViewById(R.id.title_right_btn);
-        mRightButton2 = (ImageButton) findViewById(R.id.title_right_btn_sub);
-        mRightText = (TextView) findViewById(R.id.title_right_text);
-        mLeftButton.setOnClickListener(this);
-        mLeftImage.setOnClickListener(this);
-        mCenterText.setOnClickListener(this);
-        mRightText.setOnClickListener(this);
-        mRightButton.setOnClickListener(this);
-        mRightButton2.setOnClickListener(this);
-
-    }
-
-    public void initAttrs(Context context, AttributeSet attrs) {
-
-        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.AppTitle);
-        int totalAttributes = typedArray.getIndexCount();
-        for (int i = 0; i < totalAttributes; i++) {
-            int index = typedArray.getIndex(i);
-            if (index == R.styleable.AppTitle_leftButtonRes) {
-                mLeftButton.setImageResource(getResourceId(typedArray, index, R.drawable.ic_launcher));
-            } else if (index == R.styleable.AppTitle_leftImageRes) {
-                mLeftImage.setVisibility(VISIBLE);
-                mLeftImage.setImageResource(getResourceId(typedArray, index, R.drawable.ic_launcher));
-            } else if (index == R.styleable.AppTitle_showBack) {
-                mLeftButton.setVisibility(getBoolean(typedArray, index, R.bool.default_showBack) ? VISIBLE : INVISIBLE);
-            } else if (index == R.styleable.AppTitle_leftText) {
-                mLeftText.setVisibility(VISIBLE);
-                mLeftText.setText(getString(typedArray, index));
-            } else if (index == R.styleable.AppTitle_leftTextColor) {
-                mLeftText.setTextColor(getColor(typedArray, index, R.color.default_leftTextColor));
-            } else if (index == R.styleable.AppTitle_leftTextSize) {
-                mLeftText.setTextSize(TypedValue.COMPLEX_UNIT_PX, getDimenSize(typedArray, index, R.dimen.default_leftTextSize));
-            } else if (index == R.styleable.AppTitle_centerText) {
-                mCenterText.setVisibility(VISIBLE);
-                mCenterText.setText(getString(typedArray, index));
-            } else if (index == R.styleable.AppTitle_centerTextColor) {
-                mCenterText.setTextColor(getColor(typedArray, index, R.color.default_centerTextColor));
-            } else if (index == R.styleable.AppTitle_centerTextSize) {
-                mCenterText.setTextSize(TypedValue.COMPLEX_UNIT_PX, getDimenSize(typedArray, index, R.dimen.default_centerTextSize));
-            } else if (index == R.styleable.AppTitle_rightText) {
-                mRightText.setVisibility(VISIBLE);
-                mRightText.setText(getString(typedArray, index));
-            } else if (index == R.styleable.AppTitle_rightTextColor) {
-                mRightText.setTextColor(getColor(typedArray, index, R.color.default_rightTextColor));
-            } else if (index == R.styleable.AppTitle_rightTextSize) {
-                mRightText.setTextSize(TypedValue.COMPLEX_UNIT_PX, getDimenSize(typedArray, index, R.dimen.default_rightTextSize));
-            } else if (index == R.styleable.AppTitle_rightButtonRes) {
-                mRightButton.setVisibility(VISIBLE);
-                mRightButton.setImageResource(getResourceId(typedArray, index, R.drawable.ic_launcher));
-            } else if (index == R.styleable.AppTitle_rightButton2Res) {
-                mRightButton2.setVisibility(VISIBLE);
-                mRightButton2.setImageResource(getResourceId(typedArray, index, R.drawable.ic_launcher));
-            }
-
-        }
-        typedArray.recycle();
-    }
-
-    @Override
-    public void onClick(View view) {
-        if (view.getId() == R.id.title_back_btn) {
-            ((Activity) getContext()).finish();
-        }
-        actionListener.OnActionClickListener(view);
-    }
-
 
     public interface ActionListener {
         void OnActionClickListener(View view);
+
+        boolean OnActionLongClickListener(View view);
     }
 
     public static int getResourceId(
